c02a4898f4496767382f60110a07601d859ba48c
Saleh Akram <STAkram@hotmail.co.uk>
2024-05-12 10:01:14 +0100

    initial commit

---
+++ space-invaders.py
@@ -0,0 +1,17 @@
+import sys
+import pygame
+
+
+class SpaceInvaders:
+    """Overall class to manage the game"""
+
+    def __int__(self):
+        pass
+
+    def run_game(self):
+        print("Space Invaders")
+
+
+if __name__ == "__main__":
+    si = SpaceInvaders()
+    si.run_game()
