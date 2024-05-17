e18e21a3c58b2437d7a8743d38ffb57aaf7bec95
Saleh Akram <STAkram@hotmail.co.uk>
2024-05-17 10:45:34 +0100

    game loop set up

--- space-invaders.py
+++ space-invaders.py
@@ -5,11 +5,19 @@ import pygame
 class SpaceInvaders:
     """Overall class to manage the game"""
 
-    def __int__(self):
-        pass
+    def __init__(self):
+        """initialise the game, and create game resource"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Space Invaders")
 
     def run_game(self):
-        print("Space Invaders")
+        """start main game loop"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
